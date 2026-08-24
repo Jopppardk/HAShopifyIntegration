393e6690c582337c2100c71622b55a029c0a4d42
Jopppardk <rene@sablen.dk>
2026-08-24 05:31:31 +0200

    feat: add rolling monthly comparison (custom_components/shopify_performance/const.py)

--- custom_components/shopify_performance/const.py
+++ custom_components/shopify_performance/const.py
@@ -9,6 +9,7 @@ CONF_SHOP_DOMAIN = "shop_domain"
 
 API_VERSION = "2026-07"
 UPDATE_INTERVAL = timedelta(minutes=5)
+MONTHLY_UPDATE_INTERVAL = timedelta(hours=6)
 
 DATA_COORDINATOR = "coordinator"
 
