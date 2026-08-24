2ee1137f31439d52e00bf24efce1a98f2bd0dd1b
Jopppardk <rene@sablen.dk>
2026-08-24 05:31:39 +0200

    feat: add rolling monthly comparison (custom_components/shopify_performance/__init__.py)

--- custom_components/shopify_performance/__init__.py
+++ custom_components/shopify_performance/__init__.py
@@ -9,7 +9,10 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import ShopifyApiClient
 from .const import CONF_SHOP_DOMAIN, DOMAIN
-from .coordinator import ShopifyPerformanceCoordinator
+from .coordinator import (
+    ShopifyMonthlyRevenueCoordinator,
+    ShopifyPerformanceCoordinator,
+)
 
 PLATFORMS = [Platform.SENSOR]
 
@@ -23,9 +26,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         client_secret=entry.data[CONF_CLIENT_SECRET],
     )
     coordinator = ShopifyPerformanceCoordinator(hass, client)
+    monthly_coordinator = ShopifyMonthlyRevenueCoordinator(hass, client)
     await coordinator.async_config_entry_first_refresh()
+    await monthly_coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        "coordinator": coordinator,
+        "monthly_coordinator": monthly_coordinator,
+    }
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
@@ -37,3 +45,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         if not hass.data[DOMAIN]:
             hass.data.pop(DOMAIN)
     return unload_ok
+
