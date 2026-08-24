2c0ff3bdaea6bdb7ed37b91f7cfe22934459442a
Jopppardk <rene@sablen.dk>
2026-08-24 06:18:14 +0200

    Add custom_components/shopify_integration/const.py for v0.3.0

---
+++ custom_components/shopify_integration/const.py
@@ -0,0 +1,15 @@
+"""Constants for Shopify Integration."""
+
+from datetime import timedelta
+
+DOMAIN = "shopify_integration"
+PLATFORMS = ["sensor"]
+
+CONF_SHOP_DOMAIN = "shop_domain"
+
+API_VERSION = "2026-07"
+UPDATE_INTERVAL = timedelta(minutes=5)
+MONTHLY_UPDATE_INTERVAL = timedelta(hours=6)
+
+DATA_COORDINATOR = "coordinator"
+
